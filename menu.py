eb8bc810fa5164ca07111256e26cc1b28974e9b1
Al0e5 <adek.lipek@gmail.com>
2024-01-09 19:57:12 +0100

    Adding a background to the title of game in main menu

--- menu.py
+++ menu.py
@@ -3,7 +3,9 @@ from button import Button
 from window import Window
 import functions
 
-YELLOW = (0, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 
 
 class Menu(Window):
@@ -15,10 +17,14 @@ class Menu(Window):
         play_button = Button(self.width / 2, self.height * 2 / 5, "PLAY!", 200, 50)
         rules_button = Button(self.width / 2, self.height * 3 / 5, "RULES", 200, 50)
         quit_button = Button(self.width / 2, self.height * 4 / 5, "QUIT", 200, 50)
-        running = True
         self.screen.blit(self.menu_image, (0, 0))
-        self.bliting_on_scren(functions.create_text("MONOPOLY POLAND", self.h1_font, YELLOW,
-                                                    (self.width / 2, self.height * 1 / 5)))
+        title_text = functions.create_text("MONOPOLY POLAND", self.h1_font, WHITE,(self.width / 2, self.height * 1 / 5))[0]
+        text_rect = title_text.get_rect()
+        background_surface = pygame.Surface((text_rect.width, text_rect.height))
+        background_surface.fill(RED)
+        background_surface.blit(title_text, (0, 0))
+        self.screen.blit(background_surface, (self.width / 2 - text_rect.width / 2, self.height * 1 / 5 - text_rect.height / 2))
+        running = True
         while running:
             self.screen.blit(play_button.create_surf(play_button.is_hover()), play_button.hit_box)
             self.screen.blit(rules_button.create_surf(rules_button.is_hover()), rules_button.hit_box)
