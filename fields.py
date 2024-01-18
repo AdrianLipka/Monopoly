11d8cf0e34709a54dfa1ecfa8b9226d707633ce7
AdrianLipka <adek.lipek@gmail.com>
2024-01-18 22:19:00 +0100

    Adding a rent payment mechanism and modifying the grid of elements

--- fields.py
+++ fields.py
@@ -1,5 +1,6 @@
 import pygame
 from fields_info import field_info
+import functions
 
 COLORS_OF_DISTRICTS = ({"Position": (1, 3), "Color": "Brown"},
                        {"Position": (6, 7, 9), "Color": "Light blue"},
@@ -27,7 +28,7 @@ class Field:
         self.screen = screen
         self.name = name
         self.position = position
-        self.properties = {"Occupied": False, "To buy": False}
+        self.properties = {"Occupied": False, "To buy": False, "To pay": False, "Cost": 0}
         self.owner = None
         self.rent = None
         image_path = "images/board/"
@@ -44,13 +45,18 @@ class Field:
     def buying(self, player):
         self.owner = player
         self.properties["Occupied"] = True
-        self.rent = self.properties["Rent"][0]
+        self.properties["To pay"] = True
+        self.rent = self.properties["Rent"][1]
         player.money -= self.properties["Cost"]
 
     def field_to_buy(self):
         self.properties["To buy"] = True
         title_deed_card_image = pygame.image.load(f"images/title_deed_cards/{self.name.replace(" ", "")}.png")
         self.properties["Title deed card"] = title_deed_card_image
+
+    def paying_rent(self, font, color):
+        rent_to_pay_text = functions.create_text(f"Rent paid: {functions.money_amount(self.rent)}", font, color, (1400, 700))
+        return rent_to_pay_text
 
     def __str__(self):
         return f"{self.name} ({self.position})"
@@ -70,6 +76,13 @@ class City(Field):
         self.owner.money -= self.properties["Building cost"]
         self.builded += 1
         self.rent = self.properties["Rent"][self.builded]
+
+    def buying(self, player):
+        self.owner = player
+        self.properties["Occupied"] = True
+        self.properties["To pay"] = True
+        self.rent = self.properties["Rent"][0]
+        player.money -= self.properties["Cost"]
 
     def showing_buildings(self):
         if self.builded > 0:
@@ -93,7 +106,6 @@ class City(Field):
                     case 3:
                         position[0] += 18
                         position[1] += 50
-                #position = tuple(position)
                 pygame.draw.circle(self.screen, DARK_RED, position, 10)
             else:
                 for i in range(self.builded):
@@ -117,14 +129,9 @@ class City(Field):
 class Transport(Field):
     def __init__(self, screen, position, name):
         Field.__init__(self, screen, position, name)
-        self.price = 2000000
-        self.rent = {1: 250000, 2: 500000, 3: 1000000, 4: 2000000}
+        self.properties["Cost"] = 2000000
+        self.properties["Rent"] = {1: 250000, 2: 500000, 3: 1000000, 4: 2000000}
         self.field_to_buy()
-
-    def buying(self, player):
-        self.owner = player
-        self.properties["Occupied"] = True
-        player.money -= self.price
 
 
 class CommunityChest(Field):
@@ -140,19 +147,20 @@ class ChanceCard(Field):
 class Communication(Field):
     def __init__(self, screen, position, name):
         Field.__init__(self, screen, position, name)
-        self.price = 1500000
-        self.rent = {1: 40000, 2: 100000}
+        self.properties["Cost"] = 1500000
+        self.properties["Rent"] = {1: 40000, 2: 100000}
         self.field_to_buy()
-
-    def buying(self, player):
-        self.owner = player
-        self.properties["Occupied"] = True
-        player.money -= self.price
 
 
 class IncomeTax(Field):
     def __init__(self, screen, position, name):
         Field.__init__(self, screen, position, name)
+        self.properties["To pay"] = True
+        self.rent = 2000000
+
+    def paying_rent(self, font, color):
+        rent_to_pay_text = functions.create_text(f"Rent paid: {functions.money_amount(self.rent)}", font, color, (1400, 700))
+        return rent_to_pay_text
 
 
 class Corner(Field):
